50c8d3e3d1d145c779f9f34f961ddf2c5d56b62b
Juan Sebastian Buitrago castrillon <juan.castrillon@smith.co>
2021-07-08 18:15:53 -0500

    Added logic to display content wuth animations

--- sbcowlsite/src/app/app.component.ts
+++ sbcowlsite/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'sbcowlsite';
+
+  ngOnInit(): void {
+
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+
+    let callback = (entries, observer) => {
+      entries.forEach(entry => {
+        
+        if(entry.isIntersecting){
+          entry.target.classList.remove('hidden');
+          entry.target.classList.add('fadeInRight');
+        }
+      });
+    };
+    
+    let observer = new IntersectionObserver(callback, options);
+
+    let target = document.querySelector('#observerTest');
+    observer.observe(target);
+  } 
 }
